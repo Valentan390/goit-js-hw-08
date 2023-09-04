9d541a7cd61885f36e15c36d11d0ffe0f8cf3ae3
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-09-04 22:24:22 +0300

    Сделал ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const makeGalleryCard = pictureInfo => {
+  const { preview, original, description } = pictureInfo;
+
+  return `
+   <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+       />
+     </a>
+   </li
+  `;
+};
+
+
+const galleryCardsTemplates = galleryItems.map(pictureInfo => makeGalleryCard(pictureInfo));
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsTemplates.join(''));
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionDelay: 250
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+import throttle from 'lodash.throttle';
 
+const userFormEmail = document.querySelector('.feedback-form');
+
+const userData = {};
+
+const fillContactFormFields = () => {
+
+    const userDataFromLS = JSON.parse(localStorage.getItem("feedback-form-state"));
+   
+    if (userDataFromLS === null) {
+      return;
+  }
+    for (const key in userDataFromLS) {
+        if (userDataFromLS.hasOwnProperty(key)) {
+          userFormEmail.elements[key].value = userDataFromLS[key];
+            
+        }
+    }
+    
+};
+
+fillContactFormFields();
+
+const onContactFormFieldElChange = event => {
+
+  const { target: contactFormFieldEl } = event;
+
+  const name = contactFormFieldEl.name;
+  const value = contactFormFieldEl.value;
+  
+   userData[name] = value;
+
+ localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+
+};
+
+userFormEmail.addEventListener('input', throttle(onContactFormFieldElChange, 500));
+
+const onContactFormElSubmit = event => {
+
+     const {
+        elements: { email, message }
+    } = event.currentTarget;
+
+    if (email.value === '' || message.value === '') {
+        return alert('Please fill in all the fields!');
+    };
+    const user = { Email: email.value, message: message.value };
+    
+    console.log(user);
+
+   event.preventDefault();
+   event.target.reset();
+    
+   localStorage.removeItem("feedback-form-state");
+
+};
+
+userFormEmail.addEventListener('submit', onContactFormElSubmit)
